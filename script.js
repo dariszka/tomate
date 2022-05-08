71b5ea68d71ce6ec0036477f75a5540ac049d5ae
dariszka <darinapetrova05@gmail.com>
2022-05-08 21:18:12 +0200

    Add initial structure of page.

---
+++ script.js
@@ -0,0 +1,8 @@
+// const container = document.querySelector('#container'); 
+
+// const greeting = document.createElement('div')
+//     greeting.classList.add('greeting')
+//     greeting.textContent = 'hello babe have a nice study Xx'
+//     container.appendChild(greeting)
+
+const greeting = document.querySelector('#greeting');
