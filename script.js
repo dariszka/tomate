29622d9b7ff894dde5c4ab5cba874b9b40f8e5d5
dariszka <darinapetrova05@gmail.com>
2022-05-09 17:38:04 +0200

    Edit content box with settings.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const icon = document.querySelector('#icon');
 const settings = document.querySelector('#settings');
+const menu = document.querySelector('.menu');
+const box = document.querySelector('#box');
+const timer = document.querySelector('.timer')
+
+createSettingsContents()
 
 icon.addEventListener('click', clickOnSettings)
 
-createSettingsContents()
+
 
 //////////////////////////////////////////////
 
@@ -12,29 +17,33 @@ function createSettingsContents() {
 const mode = document.createElement('div');
     mode.classList.add('mode')
     mode.textContent = 'dark/light'
-    settings.appendChild(mode)
+    menu.appendChild(mode)
 
 const workTime = document.createElement('div');
     workTime.classList.add('work')
     workTime.textContent = 'work time: '
-    settings.appendChild(workTime)
+    menu.appendChild(workTime)
 
 const breakTime = document.createElement('div');
     breakTime.classList.add('break')
     breakTime.textContent = 'break time: '
-    settings.appendChild(breakTime)
+    menu.appendChild(breakTime)
 
 const progress = document.createElement('div');
     progress.classList.add('progress')
-    progress.textContent = '/goal '
-    settings.appendChild(progress)
+    progress.textContent = '/goal'
+    menu.appendChild(progress)
 
 }
 
 function clickOnSettings() {
     icon.classList.toggle('rotate')
+
+    menu.classList.toggle('showMenu')
+    box.classList.toggle('showMenu')
+
+    timer.classList.toggle('hideTimer')
+
 } 
 
-function showSettingsContents() {
-    
-}
+
