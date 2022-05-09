72006734060b95d96af001501515693f3d887c7c
dariszka <darinapetrova05@gmail.com>
2022-05-09 16:02:53 +0200

    Create settings contents model.

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+const icon = document.querySelector('#icon');
 const settings = document.querySelector('#settings');
 
-settings.addEventListener('click', clickOnSettings)
+icon.addEventListener('click', clickOnSettings)
 
-function clickOnSettings() {
-    settings.classList.toggle('rotate')
-} 
+createSettingsContents()
+
+//////////////////////////////////////////////
 
 function createSettingsContents() {
-    //write and add every setting
 
-    //dark/light mode
-    //work length
-    //break length
-    //goal, progress
+const mode = document.createElement('div');
+    mode.classList.add('mode')
+    mode.textContent = 'dark/light'
+    settings.appendChild(mode)
+
+const workTime = document.createElement('div');
+    workTime.classList.add('work')
+    workTime.textContent = 'work time: '
+    settings.appendChild(workTime)
+
+const breakTime = document.createElement('div');
+    breakTime.classList.add('break')
+    breakTime.textContent = 'break time: '
+    settings.appendChild(breakTime)
+
+const progress = document.createElement('div');
+    progress.classList.add('progress')
+    progress.textContent = '/goal '
+    settings.appendChild(progress)
+
 }
+
+function clickOnSettings() {
+    icon.classList.toggle('rotate')
+} 
 
 function showSettingsContents() {
     
