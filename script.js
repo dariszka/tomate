162d69d5bce70460ccc243a8bc203a6dfe0155ec
dariszka <darinapetrova05@gmail.com>
2022-06-29 14:58:17 +0200

    Refactor.

--- script.js
+++ script.js
@@ -138,8 +138,8 @@ function handleTimerClick(sessionDuration, display) {
 
 function updateTimerDisplay(remainingTime, display) {
         hours = Math.floor(remainingTime / 3600) 
-        minutes = (remainingTime / 60) % 60 | 0
-        seconds = (Math.round(remainingTime)) % 60 | 0
+        minutes = Math.round(remainingTime) / 60  | 0
+        seconds = Math.round(remainingTime) % 60 | 0
         
         hours = hours < 10 ? '0' + hours : hours;
         minutes = minutes < 10 ? '0' + minutes : minutes;
